391a85900373749fa2845bad0e24b85c310bc28f
bruno jaen <brunojaen@outlook.com.br>
2022-11-18 11:31:01 -0300

    comitando alterações

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,31 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
+}
 
 
 (async () => {
 
     const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
     console.log(profileData)
-
 })
 
